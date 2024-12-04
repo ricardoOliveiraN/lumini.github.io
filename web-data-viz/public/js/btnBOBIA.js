7cb94830764b3f0ca9ad8f401e37ca07075ca3d3
ChavesCoder <nata.chaves@sptech.school>
2024-12-04 02:21:15 -0300

    Atualizão: Menu do site interno padronizado, adicionando bobIA.

---
+++ web-data-viz/public/js/btnBOBIA.js
@@ -0,0 +1,18 @@
+
+    // fazer a chamada do BobIA
+    async function gerarResposta() {
+        const pergunta = document.getElementById('pergunta').value;
+
+        const response = await fetch('http://10.18.35.237:3333/perguntar', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ pergunta })
+        });
+
+        const data = await response.json();
+
+        resposta.style.display = 'block';
+        document.getElementById('resposta').innerText = data.resultado;
+    }
