4a47f784f54a33e80cf2a612b73d26626d0922b9
ChavesCoder <nata.chaves@sptech.school>
2024-12-04 09:17:57 -0300

    Atualização: botão BOBia

--- web-data-viz/public/js/btnBOBIA.js
+++ web-data-viz/public/js/btnBOBIA.js
@@ -1,18 +1,25 @@
 
-    // fazer a chamada do BobIA
-    async function gerarResposta() {
-        const pergunta = document.getElementById('pergunta').value;
+// fazer a chamada do BobIA
+async function gerarResposta() {
+    const pergunta = document.getElementById('pergunta').value;
 
-        const response = await fetch('http://10.18.35.237:3333/perguntar', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ pergunta })
-        });
+    const response = await fetch('http://10.18.35.237:3333/perguntar', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ pergunta })
+    });
 
-        const data = await response.json();
+    const data = await response.json();
 
-        resposta.style.display = 'block';
-        document.getElementById('resposta').innerText = data.resultado;
+    resposta.style.display = 'block';
+    document.getElementById('resposta').innerText = data.resultado;
+}
+function teste(resposta) {
+    if (resposta == 1) {
+        containerBOBIA.style.display = 'block'
+    } else {
+        containerBOBIA.style.display = 'none'
     }
+}
